161c22e2e27b49a31ab1453ac579649e45362091
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:42:13 -0600

    Capitulo 14
    Uso de la función pop() para separar datos de una lista

---
+++ Practica014_21110391_6E2_Pop.py
@@ -0,0 +1,8 @@
+
+stoiarto = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colon1 = stoiarto.pop(1)
+colon2 = stoiarto.pop(7)
+
+todoesculpadelmarron = [colon1, colon2]
+
+print(todoesculpadelmarron)

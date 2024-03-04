1614665cde36e44980b1f7133705d8cd0531a36e
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:40:19 -0600

    Capitulo 12
    Uso de la función del() para borrar información en una lista

---
+++ Practica012_21110391_6E2_Del.py
@@ -0,0 +1,10 @@
+
+nadiemedicequenombreponerlealavariable = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+del nadiemedicequenombreponerlealavariable[1]
+del nadiemedicequenombreponerlealavariable[3]
+del nadiemedicequenombreponerlealavariable[4]
+del nadiemedicequenombreponerlealavariable[-3]
+
+print(nadiemedicequenombreponerlealavariable)
+
+#En efecto, nadie me prohibe ponerle "nadiemedicequenombreponerlealavariable" a la variable xd

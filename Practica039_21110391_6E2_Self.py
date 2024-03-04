2c196b4ee1c9e1fb273813f6cfc2914cda8a0adf
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:09:09 -0600

    Capitulo 39
    Uso de self dentro de la POO

---
+++ Practica039_21110391_6E2_Self.py
@@ -0,0 +1,14 @@
+
+class Morro: #Hola profe
+    def __init__(tu, n, a): #Hace tiempo 
+        tu.n = n #que no 
+        tu.a = a #nos encontramos
+    def simp(tu): #No estoy intentando
+        print('Tu nombre:', tu.n, '\nTus apellidos:', tu.a) #hacer ver que no esta
+
+u1 = Morro('Omar', 'Munguia Camacho') #Copiado profe
+u2 = Morro('Cesar', 'Carillo Alcaraz') #Esk es literalmente el anterior programa
+u2.n = 'Erland' #Pero con esto de por medio xd (<=Tambien asi se llama mi compa xd)
+u2.simp() #Asi que no se moleste si se ven igual profe xd
+
+#ni me di cuenta de que self debia ser algo en especifico hasta que lo modifique xd, pero igual funciona profe

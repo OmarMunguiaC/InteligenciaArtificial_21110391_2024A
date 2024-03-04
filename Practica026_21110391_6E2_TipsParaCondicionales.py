4bfd86cba1986bea28492698575c86ba3035c0d0
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:56:09 -0600

    Capitulo 26
    Uso de contracciones para el uso de if y else

---
+++ Practica026_21110391_6E2_TipsParaCondicionales.py
@@ -0,0 +1,5 @@
+
+x = 8642316843
+y = 84651682652
+#Izi pizi lemon sqizzi (REDACTED)(Las palabras anteriores fueron censuradas para el publico en general para el bien de la salubridad publica y de la conducta deportiva dentro de las instalaciones anteriormente mencionadas, por su atencion, gracias :))
+print('Uno es mayor que otro') if x < y else print('Otro es mayor que el otro otro')

ddc6b9c32383600f6e4947767e530671fc50ebf3
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:54:54 -0600

    Capitulo 25
    Uso de funciones if no anidadas para múltiples condiciones

---
+++ Practica025_21110391_6E2_IF_Multiple.py
@@ -0,0 +1,13 @@
+
+ent = int(input('Di un numero del 1 al 4, PORFA que sea del 1 al 4:\n'))
+
+if ent == 1:
+    print('Number 1 primo') #Maricarmen que hiciste?
+if ent == 2:
+    print('Number 2 primo') #Nada morra, nomas digo tarugadas
+if ent == 3:
+    print('Number 3 primo') #Pero eso no se puede hacer! Es ilegal!!
+if ent == 4:
+    print('Number 4 primo') #OBLIGAME
+if ent <= 0 or ent >= 5:
+    print('Como se nota que no sabes leer ijo, te dije que del 1 al 4')

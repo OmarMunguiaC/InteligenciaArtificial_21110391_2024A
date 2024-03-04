2c49bcae2b4e144db601ee763e152321ccee3db3
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:08:17 -0600

    Capitulo 38
    Uso de la palabra dedicada __init__ para la POO

---
+++ Practica038_21110391_6E2_POO-Init.py
@@ -0,0 +1,13 @@
+
+class Morro:
+        def __init__(tu, n, a): #una buena tuna pa comer profe xd
+                tu.n = n
+                tu.a = a
+        def simp(tu): #En efecto, tu eres un simp
+                print('Tu nombre:', tu.n, '\nTus apellidos:', tu.a)
+
+u1 = Morro('Omar', 'Munguia Camacho') #Este definitivamente no soy yo (guiño*)
+u2 = Morro('Cesar', 'Carrillo Alcaraz') #En efecto, este es un compa que tengo xd
+
+u1.simp() #Mostrar simp 1
+u2.simp() #Mostrar simo 2

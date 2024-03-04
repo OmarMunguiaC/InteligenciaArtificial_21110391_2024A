ae0a202e0f8dc8f8da98160a984c8588ee24a4b0
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:45:30 -0600

    Capitulo 17
    Uso de la función sort() para organizar datos de una lista

---
+++ Practica017_21110391_6E2_Ordenar.py
@@ -0,0 +1,5 @@
+
+sefuelaluuuuuuuuuuuuuuuuuzentodoelbaaaaaaaaarrio = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #La lista mas poderosa de todas
+sefuelaluuuuuuuuuuuuuuuuuzentodoelbaaaaaaaaarrio.sort(reverse=True) #acomodar
+
+print(sefuelaluuuuuuuuuuuuuuuuuzentodoelbaaaaaaaaarrio)

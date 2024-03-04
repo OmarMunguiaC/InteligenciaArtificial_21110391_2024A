3a6318137ba1449c2f82bfc22694da53f23b8afa
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:47:45 -0600

    Capitulo 19
    Aplicación y diferenciación entre una tupla y una lista

---
+++ Practica019_21110391_6E2_Tuplas.py
@@ -0,0 +1,8 @@
+
+
+tulpapatulpatulpapatulpa = ('rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print(tulpapatulpatulpapatulpa[1])
+
+porfinotralistaquenosealadecoloresarribaelbocaaaaaaaaa = (10, 1, 5, 11)
+yanosequeponerlavvd = porfinotralistaquenosealadecoloresarribaelbocaaaaaaaaa[0] + porfinotralistaquenosealadecoloresarribaelbocaaaaaaaaa[2] + porfinotralistaquenosealadecoloresarribaelbocaaaaaaaaa[3] - porfinotralistaquenosealadecoloresarribaelbocaaaaaaaaa[1] #Este comentario de aqui solo esta para ver que tanto puedo alargar esta fila, no es como si no tuviese algo que hacer la verdad, ando muy ocupado tratando de hacer esto dos dias antes de la fecha de entrega y ps como que no estoy para estos juegos, pero bueno, de regreso a trabajar xd
+print(yanosequeponerlavvd)

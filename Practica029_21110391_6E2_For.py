34703a392209b1f56e26dd3d4774e05ccc9da280
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:58:51 -0600

    Capitulo 29
    Uso y aplicación del ciclo de repetición for

---
+++ Practica029_21110391_6E2_For.py
@@ -0,0 +1,7 @@
+
+sinyolandamarikarmen = ('rojo', 'azul', 'verde', 'amarillo')
+
+print('Colores dentro del listado:\n')
+for yolo in sinyolandamarikarmen:
+	print(yolo + ' si')
+print('\ny ps hasta aki termina ijo, esperabas algo mas? xd')

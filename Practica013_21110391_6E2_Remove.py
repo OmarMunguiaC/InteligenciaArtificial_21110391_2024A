be9619b5d4749b0021decf3e1d521b711960161f
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:41:25 -0600

    Capitulo 13
    Uso de la función remove() para eliminar datos de una lista

---
+++ Practica013_21110391_6E2_Remove.py
@@ -0,0 +1,6 @@
+
+porqueelcafetieneacentomemolestaalcompilarelprogramaporfayaparen = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+porqueelcafetieneacentomemolestaalcompilarelprogramaporfayaparen.remove('amarillo')
+porqueelcafetieneacentomemolestaalcompilarelprogramaporfayaparen.remove('rojo')
+
+print(porqueelcafetieneacentomemolestaalcompilarelprogramaporfayaparen)

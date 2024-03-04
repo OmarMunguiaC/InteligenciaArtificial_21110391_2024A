497910326325af496fb4786fb0329c807082cedd
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:00:46 -0600

    Capitulo 31
    Uso y aplicación de diccionarios para guardado de múltiples datos en una sola variable

---
+++ Practica031_21110391_6E2_Diccionarios.py
@@ -0,0 +1,14 @@
+
+t1 = { #Este es el teclado 1
+	'Categoría': 'Teclados', #NOTEC NOTEC NOTEC
+	'Modelo': 'HyperX Alloy FPS Pro', #Este es el modelo que no voy a comprar pk esta muy caro
+	'Precio': '89,99' #CARO CARO CARO
+}
+
+t2 = { #Este no es el teclado 1
+	'Categoría': 'Teclados', #TEC TEC TEC
+	'Modelo': 'Corsair K55 RGB', #Este tampoco pk igual esta muy caro
+	'Precio': '59,99' #NOTANCARO NOTANCARO NOTANCARO
+}
+
+print('El modelo', t2['Modelo'], 'cuesta', t2['Precio'], '$.') #MUESTRATE

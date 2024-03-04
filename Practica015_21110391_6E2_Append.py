8c0f732e413a2e07762b899e29a466fcfbfd5c35
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:43:20 -0600

    Capitulo 15
    Uso de la función append() para agregar datos a una lista

---
+++ Practica015_21110391_6E2_Append.py
@@ -0,0 +1,6 @@
+
+mismalistamismatristeza = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+mismalistamismatristeza.append('fuxia') #Definitivamente no estoy copeando profe
+mismalistamismatristeza.append('celeste') #Es parte de la experiencia CETI
+
+print(mismalistamismatristeza)

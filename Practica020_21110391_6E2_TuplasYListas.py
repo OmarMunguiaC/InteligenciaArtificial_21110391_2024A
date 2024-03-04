9f13bd8cd14eae868bf829c6746bc8ce6a187ce3
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:48:40 -0600

    Capitulo 20
    Transformación de una tupla a lista y viceversa

---
+++ Practica020_21110391_6E2_TuplasYListas.py
@@ -0,0 +1,5 @@
+
+nopuedeservolvemosalamismaLISTAAAAAAAAAAAAA = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+aaaaaaaaaaaaaqueeratuplaynotulpaxd = tuple(nopuedeservolvemosalamismaLISTAAAAAAAAAAAAA)
+
+print(type(aaaaaaaaaaaaaqueeratuplaynotulpaxd))

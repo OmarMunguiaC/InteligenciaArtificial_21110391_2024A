2cb6a3994be5d7d37db2e83f4f46cc537d27f98b
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:51:27 -0600

    Capitulo 22
    Uso de la función else para condiciones no cumplidas

---
+++ Practica022_21110391_6E2_IF_ELSE.py
@@ -0,0 +1,9 @@
+
+#Se supone que corregimos esto
+c = 'color_sangre de mis enemigos'
+if c != 'rojo':
+    print("No es rojo paps") 
+else:
+    print("El color si es rojo paps")
+
+#Teoricamente si es rojo pero ps xd

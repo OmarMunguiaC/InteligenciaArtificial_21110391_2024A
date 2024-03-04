21ea16d722911372d3e56e0346ba15fa5f3fa666
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:46:30 -0600

    Capitulo 18
    Uso de la función len() para contar la cantidad de datos dentro de una lista

---
+++ Practica018_21110391_6E2_Contar.py
@@ -0,0 +1,4 @@
+
+diosestamuertoynosotroslomatamos = ["avr","vamos a poner cosas","avr si si sale","noce",'que no es "no se"','es no "c"',"osease que no hay","no existe","guayaba"]
+qty = len(diosestamuertoynosotroslomatamos)
+print(qty)

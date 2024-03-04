da2d23662dd8d368b07d8231fa32acc0c831d517
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:11:01 -0600

    Capitulo 41
    Uso de las clases heredadas y sus aplicaciones

---
+++ Practica041_21110391_6E2_Herencia.py
@@ -0,0 +1,17 @@
+
+class Morro: #pass
+    def __init__(tu, n, a): #pass
+        tu.n = n #pass
+        tu.a = a #pass
+    def simp(tu): #pass
+        print('Tu nombre:', tu.n, '\nTus apellidos:', tu.a) #pass
+
+class MorroPremium(Morro): #mismo Mismo programa que el anterior
+    pass #Pero con esta otra cosa mas aqui profe xd (#smash)
+
+u1 = Morro('Omar', 'Munguia Camacho') #pass
+u2 = Morro('Cesar', 'Carillo Alcaraz') #pass
+u2.n = 'Erland'
+
+u1.simp() #pass
+u2.simp() #pass

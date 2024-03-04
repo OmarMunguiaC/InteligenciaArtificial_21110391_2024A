5da8ff030b5f50ede6c783570a1f982259c34838
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:57:53 -0600

    Capitulo 28
    Uso del ciclo de repetición while con condicionales

---
+++ Practica028_21110391_6E2_While_If.py
@@ -0,0 +1,16 @@
+
+helicopteroapachedeturbocompresiondoble = 0
+
+while helicopteroapachedeturbocompresiondoble <= 30:
+	helicopteroapachedeturbocompresiondoble += 1  
+
+	if helicopteroapachedeturbocompresiondoble == 4 or helicopteroapachedeturbocompresiondoble == 6 or helicopteroapachedeturbocompresiondoble == 10:
+		print('Se saltó el valor ', helicopteroapachedeturbocompresiondoble, ' de helicopteroapachedeturbocompresiondoble')
+		continue #la rosalia
+
+	if helicopteroapachedeturbocompresiondoble == 15:
+		print('Se rompio la ejecución del bucle cuando helicopteroapachedeturbocompresiondoble valia: ', helicopteroapachedeturbocompresiondoble)
+		break #se rompio xd
+
+	print(helicopteroapachedeturbocompresiondoble)
+#yo solo quiero ver sangre, y no voy a ir al rastro por ello xd

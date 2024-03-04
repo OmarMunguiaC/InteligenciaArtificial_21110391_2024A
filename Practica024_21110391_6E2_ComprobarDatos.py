2884e790aa8ad8e060eb74bdfbc9babbacac5cf7
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:53:32 -0600

    Capitulo 24
    Uso del if anidado para múltiples condiciones

---
+++ Practica024_21110391_6E2_ComprobarDatos.py
@@ -0,0 +1,14 @@
+
+c = input('Introduce un color:\n')
+t = ('rojo', 'azul', 'verde', 'amarillo')
+#cambiar el texto y el nombre de las variables es mi pasion
+if c in t[0]: #con ello nada me falta xd
+	print('Sista el Rojo ijo')
+elif c in t[1]:
+	print('Sista el azul ijo')
+elif c in t[2]:
+	print('Sista el verde ijo')
+elif c in t[3]:
+	print('Sista el amarillo ijo')
+else:
+	print('no ps no sta tu color we')

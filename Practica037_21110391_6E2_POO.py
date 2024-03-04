24088fbe9dd2ff33de51023f26a677c8113b0d68
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:07:06 -0600

    Capitulo 37
    Aplicación y uso de objetos y clases en la Programación Orientada a Objetos

---
+++ Practica037_21110391_6E2_POO.py
@@ -0,0 +1,13 @@
+
+class Morro: #Definir clase
+	nom = ''
+	ape = ''
+
+	def simp(tu): #definir el objeto, en este caso, el SIMP que eres
+		print('Nombre:', tu.nom, '\nApellidos:', tu.ape)
+
+u001 = Morro() 
+u001.nom = 'Omar'
+u001.ape = 'Munguia Camacho'
+
+u001.simp() #Hacemos que el simp se defina (alch la waifu se respeta profe xd)

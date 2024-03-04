d160f13de699808db6d7c77f2db7915625ae7d63
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:49:59 -0600

    Capitulo 21
    Aplicación de la función if para uso de condicionales

---
+++ Practica021_21110391_6E2_IF.py
@@ -0,0 +1,15 @@
+
+n1 = 15 #Necesito guardar las mismas condiciones
+n2 = 20 #Para que el ejercicio sea igual al que nos pide
+if n1 != n2: #No me mate profe
+	print('Se Ejecuta 1') #:)
+	
+n3 = 1450 #maaaaaaaaaaas
+n4 = 60
+if n3 > n4:
+	print('Se Ejecuta 2')
+	
+n5 = 60 #MAAAAAAAAASSSSSSSSSS
+n6 = 60
+if n5 != n6:
+	print('Se Ejecuta 3') 

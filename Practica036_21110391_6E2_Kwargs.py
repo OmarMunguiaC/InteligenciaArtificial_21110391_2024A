12053e76cc01a31f9a8119588b3d23a5df62cf0d
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:05:55 -0600

    Capitulo 36
    Uso de los kwargs dentro de una función

---
+++ Practica036_21110391_6E2_Kwargs.py
@@ -0,0 +1,5 @@
+
+def coloniadeinsectosasesinospatentadosporlaunam (**kwargs):
+	print("Este es el color " + kwargs['col2'] + '.') #Kwarg suena bien macabro profe
+
+coloniadeinsectosasesinospatentadosporlaunam(col1='rojo', col2='azul') #Las coles son saludables profe

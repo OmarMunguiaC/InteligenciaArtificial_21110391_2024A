71610476f1963cdd2f1a0b10ce47435d251bd430
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:44:20 -0600

    Capitulo 16
    Uso de la función insert() para ingresar datos a una lista de forma especifica

---
+++ Practica016_21110391_6E2_Insert.py
@@ -0,0 +1,6 @@
+
+yanoquieroestalisaprofeyameaburri = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+yanoquieroestalisaprofeyameaburri.insert(-4, 'magenta')
+yanoquieroestalisaprofeyameaburri.insert(-1, 'turquesa')
+
+print(yanoquieroestalisaprofeyameaburri) #Me voy a dar de baja de la vida alch

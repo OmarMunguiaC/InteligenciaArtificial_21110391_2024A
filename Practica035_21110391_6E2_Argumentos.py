8837d767f68447d655f77e1ac227f8b6886bae10
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:04:59 -0600

    Capitulo 35
    Definición de argumentos dentro de una función

---
+++ Practica035_21110391_6E2_Argumentos.py
@@ -0,0 +1,17 @@
+
+def colon(*args): #Chamuscado
+	pass #smash
+
+colon('rojo', 'azul', 'verde', 'amarillo') #Cuatro argumentos pa
+colon('lila', 'negro', 'rojo') #Tres argumentos pa
+colon('rosa') #Un argumento pa
+colon('marrón', 'naranja') #Dos argumentos pa
+
+def colopaper3000(*args): #NAVI
+	print('El color', args[0], 'es mi favorito.', 'pero el sabor a', args[1], 'tampoco está mal eh? xd') #Papaya
+colopaper3000('SANGRE', 'DEMOCRACIA') #Tomate
+
+def s(*args): #diorama
+	res = args[0] + args[1] + args[2] + args[3] + args[4] #Profeeeeeeee, me dan errores de NON-UTF-8, no se si es por el visual studio o soy yo que estoy bien wey xdnt
+	print('El resultado total de sus valores es:', res) #Camaron
+s(100, 23, 145, 0, 654, 2654892465) #Tequila

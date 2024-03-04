1bfbc081ad149d995d35a3aa481addcd2f0f519e
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:01:45 -0600

    Capitulo 32
    Uso del ciclo for para impresión de múltiples diccionarios

---
+++ Practica032_21110391_6E2_DiccionariosEnBucle.py
@@ -0,0 +1,15 @@
+
+t1 = { #aki inserte texto pa que no nos muramos
+	'Categoría': 'Teclados', #aki inserte texto pa que no nos muramos
+	'Modelo': 'HyperX Alloy FPS Pro', #aki inserte texto pa que no nos muramos
+	'Precio': '89,99' #aki inserte texto pa que no nos muramos
+}
+
+teclado2 = { #aki inserte texto pa que no nos muramos
+	'Categoría': 'Teclados', #aki inserte texto pa que no nos muramos
+	'Modelo': 'Corsair K55 RGB', #aki inserte texto pa que no nos muramos
+	'Precio': '59,99' #aki inserte texto pa que no nos muramos
+}
+
+for notx in t1: #aki inserte texto pa que no nos muramos
+	print(notx, '=', t1[notx] + '.') #aki inserte texto pa que no nos muramos

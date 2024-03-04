bdb40f22c2df8e0f9693b77e3e76469164b7c8f2
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:17:40 -0600

    Capitulo 45
    Uso de la librería datetime para carga de fechas

---
+++ Practica045_21110391_6E2_Datetime.py
@@ -0,0 +1,8 @@
+
+import datetime #Libreria FRCHA, k original xd
+
+fetiche = datetime.datetime.now() #Eh profe, me estan robando los datos xdnt
+print(fetiche) #Kfeo fetiche
+
+fechoria = datetime.datetime(2024, 3, 3, 23, 59) #Las fechorias son malas
+print(fechoria) #No lo hagas xd

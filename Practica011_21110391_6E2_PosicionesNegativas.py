990fb839862bf285c48eb2bac819fc06638f3c31
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:39:34 -0600

    Capitulo 11
    Uso de valores negativos para aplicaciones en listas

---
+++ Practica011_21110391_6E2_PosicionesNegativas.py
@@ -0,0 +1,9 @@
+
+col = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(col[-1]) #Naranja
+print(col[-7]) #Amarillo
+print(col[-5]) #Lila
+print(col[-2]) #Blanco
+print(col[-10]) #Rojo
+
+#Tengo weba profe

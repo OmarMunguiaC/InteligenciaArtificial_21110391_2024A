2fe18b95e54e48561df1b0b50cd60a222d77ad82
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:32:57 -0600

    Capitulo 4
    Concatenación de palabras e impresiones

---
+++ Practica004_21110391_6E2_Concatenacion.py
@@ -0,0 +1,18 @@
+p1 = "Juntar" + " Frase"
+print(p1)
+
+p2 = "Dos"
+p3 = " Palabras"
+p4= p2+p3
+print(p4)
+
+p5 ="Palabra a"
+print(p5 + " medias")
+
+nom = "Omar"
+ap1 = "Munguia"
+ap2 = "Camacho"
+nomc = nom +" "+ ap1 +" "+ ap2
+print(nomc)
+
+

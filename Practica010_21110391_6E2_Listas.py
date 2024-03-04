90bad567bc071abdf3ce9aa9323a075884fb3f3e
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 19:38:47 -0600

    Capitulo 10
    Uso de listas y conjunto de datos

---
+++ Practica010_21110391_6E2_Listas.py
@@ -0,0 +1,8 @@
+
+col = ["rojo", "azul", "verde", "amarillo", "marron", "lila", "negro", "rosa"] #Lista de colores
+print(col[3]) #color Amarillo
+print(col[0]) #color Rojo
+print(col[7]) #color Rosa
+
+num = ["tres", "dos", "cinco", "cuatro", "uno"] #Lista del ejercicio
+print(num) #imprimir

49868db617acba247e94c45fbbe4f6f427272092
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:14:19 -0600

    Capitulo 44
    Uso de los módulos de la librería math en una función

---
+++ Practica044_21110391_6E2_Modulos.py
@@ -0,0 +1,10 @@
+
+import math #Importar la libreria math 
+
+def a1(rad1): #Aqui lo define como una funcion
+	res = math.pi * rad1 * rad1 #operacion
+	print(res) #imprimir
+a1(2) #Aqui se le da el valor al radio
+
+a2 = lambda rad2: (math.pi * rad2 * rad2) #Operacion
+print(a2(2)) #aqui tambien se le da el valor al raido (que no radio, raido xd)

d15d776ef0513fa39fd381543db0d3fa08d2219a
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:21:54 -0600

    Capitulo 48
    Uso de la función findall para búsqueda de strings en una variable

---
+++ Practica048_21110391_6E2_FindAll.py
@@ -0,0 +1,6 @@
+
+import re #Libreria RegEx
+
+palabrita = "avrsisimepuedesencontrarvamosaprobarelpoderdeestacosaavrsisimuyvrgaspuedecontodoyconmitareaymisproblemasemocionalesxd"
+busk = re.findall("puede", palabrita) #Misma cosa rara pero con otra funcion xd
+print(busk) #Spoiler: Si pudo xdxdxd

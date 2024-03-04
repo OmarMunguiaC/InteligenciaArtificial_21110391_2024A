b735e99f0fd4a755792f2a8c1ea605b7c560e1e8
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:02:49 -0600

    Capitulo 33
    Uso de funciones del() para borrar información dentro de un diccionario

---
+++ Practica033_21110391_6E2_MetodosConDiccionarios.py
@@ -0,0 +1,17 @@
+
+t1 = { 
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro', #Estoy cansado jefe...
+	'Precio': '89,99'
+}
+ 
+t2 = { 
+	'Categoría': 'Teclados', 
+	'Modelo': 'Corsair K55 RGB', #cansado... de tanto copiar...
+	'Precio': '59,99'
+}
+
+del t1 #DIGO DIGO, de tanto viajar... xd
+del t2['Categoría'] #No
+del t2['Precio'] #kiero
+print(t2['Modelo']) #reprobar :(

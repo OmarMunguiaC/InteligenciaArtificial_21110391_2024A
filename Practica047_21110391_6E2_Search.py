065ef74310e18a5b6e59f159418d88dcec9cb49b
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:20:23 -0600

    Capitulo 47
    Uso de la función search de la librería re para búsqueda de datos en una variable

---
+++ Practica047_21110391_6E2_Search.py
@@ -0,0 +1,6 @@
+
+import re #Libreria RegEx
+
+palabrota = "avrsisimepuedesencontrarvamosaprobarelpoderdeestacosaavrsisimuyvrgaspuedecontodoyconmitareaymisproblemasemocionalesxd"
+busk = re.search("poder", palabrota) #Jaja, dise bus-ca, busk, entiende? xd, Esquizofrenia hermano xdxdxd
+print(busk) #Spoiler: Si pudo xdxdxd

fa8793c31e233732f0e3e048cea72573b630ef5a
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-03-03 20:10:07 -0600

    Capitulo 40
    Declaración de clases vacías en la POO

---
+++ Practica040_21110391_6E2_ClaseVacia.py
@@ -0,0 +1,5 @@
+
+class LaClaseEstaVacia: #Significa que no hay nada
+    pass #no existe xd
+del Cosa.Coso #todo fue parte de su imaginacion #Sirve pa borrar elemento especifico
+del Cosa #La calificacion es despreciable siempre que sea mayor a 70 xd #Sirve para eliminar todo el objeto
